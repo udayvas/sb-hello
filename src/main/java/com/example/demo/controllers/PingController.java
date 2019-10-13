f211e76c6b0247d26437b281aa9b860ed0c1c30e
Uday Vashisht <uvas@Udays-MacBook-Pro.local>
2019-10-13 13:54:47 -0700

    lombok

--- src/main/java/com/example/demo/controllers/PingController.java
+++ src/main/java/com/example/demo/controllers/PingController.java
@@ -5,16 +5,21 @@ import java.time.Instant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class PingController {
 
 	@GetMapping("/")
 	public String hello() {
+		log.info("demo ready");
 		return "Hello there !! ";
 	}
 	
 	@GetMapping("/ping")
 	public String ping() {
+		log.info("ping received at "+Instant.now());
 		return "Ping at "+Instant.now();
 	}
 	
